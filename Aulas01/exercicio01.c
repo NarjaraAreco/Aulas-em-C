81f187943f3e2f35e28f367a1ee3686f5fac60ff
NarjaraAreco <narjaracatherine@gmail.com>
2024-08-22 21:09:17 -0300

    upp

--- Aulas01/exercicio01.c
+++ Aulas01/exercicio01.c
@@ -8,14 +8,13 @@ int main(){
 
     printf("Digite sua idade: ");
     scanf("%i", &idade);
-    printf("Digite sua altura: ");
-    scanf("%f", &altura);
     printf("Digite seu sexo: ");
     scanf("%s", &sexo);
+    printf("Digite sua altura: ");
+    scanf("%f", &altura);
 
-    printf("\nSua idade eh %i, sua altura eh %f e seu sexo eh %c", idade,altura,sexo);
 
-
+    printf("\nSua idade eh %i, sua altura eh %.2f e seu sexo eh %c", idade,altura,sexo);
 
     return 0;
 }
